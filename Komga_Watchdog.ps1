d1438cc940bb091b900fe142d94bfe1934b6ced9
Schmurtz <schmurtz@gmail.com>
2025-03-06 23:35:29 +0100

    Manage immediate launch

--- Komga_Watchdog.ps1
+++ Komga_Watchdog.ps1
@@ -7,7 +7,8 @@ function Get-AppPath($AppName) {
         # Retrieve the execution URI from shell:AppsFolder
         $App = Get-StartApps | Where-Object { $_.Name -like "*$AppName*" }
         return $App.AppID
-    } else {
+    }
+    else {
         Write-Host "The application '$AppName' is not installed."
         return $null
     }
@@ -20,39 +21,41 @@ function Is-AppRunning($ProcessName) {
 }
 
 # Function to restart the application if not running
-function Start-App($AppName, $ProcessName) {
+function Start-App($AppName, $ProcessName, $delay) {
     $AppID = Get-AppPath $AppName
     if ($AppID) {
         $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        $logMessage = "$timestamp - $AppName is not running. (Re)starting..."
+        $logMessage = "$timestamp - $AppName is not running. (Re)starting in $delay second(s)..."
         Write-Host $logMessage
         Add-Content -Path "app_monitor.log" -Value $logMessage
+        Start-Sleep -Seconds $delay
         # Launch the UWP application via explorer.exe
         Start-Process "explorer.exe" "shell:AppsFolder\$AppID"
-    } else {
+    }
+    else {
         Write-Host "Error: Unable to find $AppName."
     }
 }
 
-# Infinite loop to monitor the application
+
 $AppName = "Komga"         # Change this to your desired application name
 $ProcessName = "Komga"      # Change this to the process name of the application
 $Host.UI.RawUI.WindowTitle = "$AppName WatchDog"
 
-    if (-not (Is-AppRunning $ProcessName)) {
-        Start-App $AppName $ProcessName
-    }
-	else
-	{
-		$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Write-Host "$timestamp - $AppName is already running."
-	}
+if (-not (Is-AppRunning $ProcessName)) {
+    Start-App $AppName $ProcessName 0
+    Start-Sleep -Seconds 5
+}
+else {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "$timestamp - $AppName is already running."
+}
 
 
+# Infinite loop to monitor the application
 while ($true) {
     if (-not (Is-AppRunning $ProcessName)) {
-        Start-App $AppName $ProcessName
-        Start-Sleep -Seconds 180  # Wait 2 minutes in case an update is in progress
+        Start-App $AppName $ProcessName 180 # Wait in case an update is in progress
     }
-    Start-Sleep -Seconds 10  # Wait 60 seconds before checking again
+    Start-Sleep -Seconds 60  # Wait interval in seconds before checking again
 }
